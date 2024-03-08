5750c65b08797037288dd280ad6f226516d6fb5c
JokerOfDarkness <darkclearwing@gmail.com>
2024-03-08 17:59:24 +0700

    Add DSA06029

--- DSA06029.cpp
+++ DSA06029.cpp
@@ -1,5 +1,19 @@
 #include<bits/stdc++.h>
 
+void selectionSort(int a[], int n){
+    std::vector<std::vector<int>> v;
+    for(int i = 1; i < n; i++){
+        int pivot = a[i];
+        int pos = i-1;
+        while(pos >= 0 && pivot < a[pos]){
+            a[pos + 1] = a[pos];
+            --pos;
+        }
+        pos++;
+        a[pos] = pivot;
+    }
+}
+
 int main(){
     int n;
     std::cin >> n;
@@ -7,5 +21,6 @@ int main(){
     for(int i = 0; i < n; i++){
         std::cin >> a[i];
     }
-    for(int )
+    selectionSort(a, n);
+
 }
