331cbc62ef05d4499cd851d55b6a96714389088e
JokerOfDarkness <darkclearwing@gmail.com>
2024-03-20 23:30:41 +0700

    Add DSA01002

--- DSA01002.cpp
+++ DSA01002.cpp
@@ -1,11 +1,36 @@
 #include<bits/stdc++.h>
 
+void tapcon(int a[], int n, int k){
+    int i = k;
+    while(i > 0 && a[i] == n-k+i){
+        i--;
+    }
+    if(i==0){
+        for(int i = 1; i <= k; i++){
+            std::cout << i << " ";
+        }
+    }
+    else{
+        a[i]++;
+        for(int j = i+1; j <= k; j++){
+            a[j] = a[j-1]+1;
+        }
+        for(int j = 1; j <= k; j++){
+            std::cout << a[j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
 int main(){
     int t;
     std::cin >> t;
     while(t--){
         int n, k;
         std::cin >> n >> k;
-        int a[]
+        int a[n];
+        for(int i = 1; i <= k; i++){
+            std::cin >> a[i];
+        }
+        tapcon(a, n, k);
     }
 }
